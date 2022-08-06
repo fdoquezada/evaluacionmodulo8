47fe1cd9a2a3652bb9bd10ee50a9f8704bd0ab7c
fernandres <fdoquezadapuno@gmail.com>
2022-08-06 02:02:52 -0400

    actualizacion de caperetas y imagenes

--- evaluacionmodulo8/index.js
+++ evaluacionmodulo8/index.js
@@ -5,8 +5,12 @@ require("dotenv").config();
 
 //iniciamos el server
 app.listen(process.env.APP_PORT,function(){
+    console.log();
     console.log("***************"+
     chalk.cyan.inverse("servidor iniciado en puerto "+
     process.env.APP_PORT)
     +"***************")
 })
+
+
+
